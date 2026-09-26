0ca9cd55e27a76c49fb1089a0d480d63a30ad40b
Vinicius Leandro Codogno <viniciusleandrocodogno@gmail.com>
2026-09-26 00:52:18 +0000

    finish `/apí/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  const postgresVersion = "16.0";
+  expect(responseBody.dependencies.database.version).toEqual(postgresVersion);
+
+  const maxConnections = 100;
+  expect(responseBody.dependencies.database.max_connections).toEqual(
+    maxConnections,
+  );
+
+  const openedConnections = 1;
+  expect(responseBody.dependencies.database.opened_connections).toEqual(
+    openedConnections,
+  );
 });
